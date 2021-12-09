a800d0c1b81def35a77d2a038fd9c63bfa3db235
securefett <thesecurian1@gmail.com>
2021-12-09 00:54:14 -0800

    no idea
    I legit have no idea what this update is but I'm scared that if I don't push it it'll yell at me

--- src/Launcher/Projectile/ProjectileRigid.gd
+++ src/Launcher/Projectile/ProjectileRigid.gd
@@ -56,7 +56,7 @@ func _on_RigidBody2D_body_entered(body):
 		body.add_child(newTower)
 		newTower.global_position = global_position
 		newTower.set_rotation(newTower.get_position().angle() + PI / 2)
-		newTower.set_position((newTower.position / newTower.position.length()) * 520)
+		newTower.set_position((newTower.position / newTower.position.length()) * 520) #Sets vector length to 520pixels
 		newTower.get_node(@"Sprite").set_texture(texture)
 		queue_free()
 	elif body is Tower:
