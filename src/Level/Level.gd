a0e8a91316ebf5b7d4bdf4457a7937bda4876eea
securefett <thesecurian1@gmail.com>
2021-12-09 22:34:58 -0800

    Colony Update
    Can now spawn n number of colonies around the planet

--- src/Level/Level.gd
+++ src/Level/Level.gd
@@ -3,30 +3,34 @@ extends Node2D
 const colony = preload("res://src/Planet/Colony/Colony.tscn")
 
 onready var planet = $Planet
+onready var raycast = $Planet/RayCast2D
+onready var colonyTimer = $Colony_Timer
+
+export (int) var numberOfColonies 
 
 func _ready():
-	spawn_colony(0)
+	randomize()
+	spawn_colonies()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	rand_range(0, 2*PI)
+#func _process(delta):
+#	pass
+
 
 func spawn_colonies():
-	pass
+	if get_tree().get_nodes_in_group("Colonies").size() < numberOfColonies:
+		spawn_colony(rand_range(0, 2 * PI))
 
 
 func spawn_colony(angle):
-	var newColony = colony.instance()
-	planet.add_child(newColony)
-	newColony.set_position(Vector2(cos(angle)*512,sin(angle)*512))
-	newColony.set_rotation(angle + PI / 2)
-	newColony.add_to_group("Colonies")
-
-
-#		var newTower = tower.instance()
-#		var texture = get_node(@"Sprite").get_texture()
-#		body.add_child(newTower)
-#		newTower.set_rotation(body.get_node(@"Sprite").get_rotation())
-#		newTower.set_position(Vector2(0,-16))
-#		newTower.get_node(@"Sprite").set_texture(texture)
+	var spawnSpot = Vector2(cos(angle)*512,sin(angle)*512)
+	raycast.set_cast_to(spawnSpot)
+	raycast.force_raycast_update()
+	if !raycast.is_colliding():
+		var newColony = colony.instance()
+		planet.add_child(newColony)
+		newColony.set_position(spawnSpot)
+		newColony.set_rotation(angle + PI / 2)
+		newColony.add_to_group("Colonies")
+	colonyTimer.start()
