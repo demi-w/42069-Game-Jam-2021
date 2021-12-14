1472647210b91146178990dc463b65329dbd5eaf
securefett <thesecurian1@gmail.com>
2021-12-14 15:33:53 -0800

    Predictor Updates
    I failed to increase sim speed of the projectile

--- src/Predictor/Predictor.gd
+++ src/Predictor/Predictor.gd
@@ -15,10 +15,14 @@ func _default_texture():
 func _default_collision():
 	return "res://src/Predictor/Defaults/DefaultPredict.tres"
 
+func _default_sim_speed():
+	return 1
+
 var defaultPredictParameters = {
 	"gravity_force" : funcref(self, "_default_gravity_force"),
 	"texture" : funcref(self, "_default_texture"),
 	"collision" : funcref(self, "_default_collision"),
+	"sim_speed" : funcref(self, "_default_sim_speed"),
 	"launch_position" : null,
 	"velocity" : null
 }
@@ -27,11 +31,10 @@ var defaultPredictParameters = {
 var _gravity_force = 1
 var _texture = null
 var _collision = null
+var _sim_speed = 1
 var _velocity = Vector2(0,0)
 var _launch_position = Vector2(0,0)
 
-func _ready():
-	print(_planet)
 
 func predict(params : Dictionary):
 	for param in defaultPredictParameters.keys():
@@ -46,9 +49,9 @@ func predict(params : Dictionary):
 func launch():
 	var phantom = projectilePhantom.instance()
 	add_child(phantom)
-	phantom.set_gravity_scale(_gravity_force)
+	phantom.set_gravity_scale(_gravity_force*_sim_speed*_sim_speed)
 	phantom.set_texture(_texture)
 	phantom.set_collision(_collision)
 	phantom.add_to_group("Paths")
 	phantom.set_position(_launch_position)
-	phantom.launch(_velocity)
+	phantom.launch(_velocity*_sim_speed)
