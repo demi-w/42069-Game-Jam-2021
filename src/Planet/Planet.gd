6a40a55ce6a6b13010d61855547c78076f0e4d7f
securefett <thesecurian1@gmail.com>
2021-12-16 01:19:37 -0800

    Planet Update
    Got rid of planetScale function for redundancy

--- src/Planet/Planet.gd
+++ src/Planet/Planet.gd
@@ -4,22 +4,17 @@ class_name Planet
 
 onready var planetCollision = $Planet_Radius
 
-export var planetScale = 1.0 setget set_scale
 var planetRadius setget set_radius, get_radius
 
 
 func _draw():
 	if Engine.is_editor_hint():
 		draw_circle(position,planetRadius,Color.red)
-	
 
-func set_scale(new_scale):
-	planetRadius = $Planet_Radius.get_shape().get_radius()
-	if Engine.is_editor_hint():
-		update()
 
 func set_radius(value):
 	planetRadius = $Planet_Radius.get_shape().get_radius()
+
 
 func get_radius():
 	return $Planet_Radius.get_shape().get_radius()
