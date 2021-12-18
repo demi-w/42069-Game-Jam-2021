c8093f83378e889a9b21afc71b0bb9d39db0bc1a
hbgg <65977881+hbgg@users.noreply.github.com>
2021-12-17 22:39:40 -0800

    Asteroid Collision Math
    what it be saying in da title

--- src/Asteroid/Asteroid.gd
+++ src/Asteroid/Asteroid.gd
@@ -37,6 +37,7 @@ var defaultParamFuncs = {
 # var a = 2
 # var b = "text"
 
+var simRadius = 5
 var timeAlive = 0
 var deathTime
 var _asteroidHash
@@ -62,7 +63,10 @@ func setupParameters(params : Dictionary, configInfo : Dictionary):
 func _ready():
 	deathTime = calc_death_time()
 	give_up_coroutine()
-	position = get_position_at_time(0)*_worldScale
+	var rotationAsPercent = _posRotation / PI / 2.0
+	timeAlive = rotationAsPercent / _period
+	_initDist += rotationAsPercent * _linearFall
+	position = get_position_at_time(timeAlive)*_worldScale
 
 func _process(delta):
 	timeAlive += delta
@@ -71,8 +75,8 @@ func _process(delta):
 	
 func get_position_at_time(time):
 	var periodTime = time*_period #Default period takes 1 second
-	return Vector2(_initDist*cos(2*PI*periodTime)-_linearFall*periodTime*cos(2*PI*periodTime),
-					_initDist*sin(2*PI*periodTime)-_linearFall*periodTime*sin(2*PI*periodTime)).rotated(_posRotation)
+	return Vector2(_initDist*cos(TAU*periodTime)-_linearFall*periodTime*cos(TAU	*periodTime),
+					_initDist*sin(TAU*periodTime)-_linearFall*periodTime*sin(TAU*periodTime))
 	
 func calc_death_time():
 	if(_initDist-_giveUpDist == 0):
@@ -87,11 +91,37 @@ func give_up_coroutine():
 	#print("AHH SHOULD GIVE UP NOW")
 	die()
 	
+
 func die():
 	get_parent().remove_asteroid(self)
 	dead = true
 	add_child(asteroidPhysics.instance())
 	#get_parent().remove_child(self)
+
+func time_intersect_ray(rads,rayRange):
+	var timeToGetToRads = rads/(TAU*_period)
+	var rotationsToGetToRange = ceil((1-rayRange-rads*_linearFall/TAU)/_linearFall)/_period
+	return timeToGetToRads + rotationsToGetToRange
+
+func time_intersect_cone(startRads,endRads,coneRange):
+	var endInTOfStart = fmod(endRads-startRads,TAU)
+	var collisionInTOfStart = fmod((1-coneRange)/_linearFall,1)*TAU
+	var collisionAngle = fmod(clamp(collisionInTOfStart,0,endInTOfStart) + startRads,TAU)
+	return time_intersect_ray(collisionAngle,coneRange)
+	
+func time_intersect_asteroid(other):
+	var a = ((simRadius+other.simRadius-_initDist+other._initDist) / 
+		(other._linearFall*other._initDist - _linearFall*_initDist))
+	
+	a = a if a > 0 else float("inf")
+	
+	var b = ((-simRadius-other.simRadius-_initDist+other._initDist) / 
+		(other._linearFall*other._initDist - _linearFall*_initDist))
+	
+	b = b if b > 0 else float("inf")
+	
+	return min(a,b)
+
 
 #Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
