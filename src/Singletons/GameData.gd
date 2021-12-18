d700ade4c2457e2429e41e7ad7240f9b622ee3a4
securefett <thesecurian1@gmail.com>
2021-12-18 14:30:38 -0800

    GameData
    Gamedata

--- src/Singletons/GameData.gd
+++ src/Singletons/GameData.gd
@@ -2,6 +2,7 @@ extends Node
 
 
 var current_level = null
+var asteroid_spawner = null
 var stardust = 0.0
 var planet_health = 100.0
 var refinery_count = 0
@@ -9,10 +10,12 @@ var refinery_count = 0
 
 func set_things(level_node):
 	current_level = level_node
+	asteroid_spawner = level_node.get_node("AsteroidSpawner")
 
 
 func reset():
 	current_level = null
+	asteroid_spawner = null
 	stardust = 0.0
 	planet_health = 100.0
 	refinery_count = 0
